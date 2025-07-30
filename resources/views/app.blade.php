9ea3b38c39db475752443423322be02a595a5c7d
Sylva-Egb <romaricegbewole@gmail.com>
2025-07-30 01:49:43 +0100

    updating the app php to fix the payload problem

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,10 +12,8 @@
 
         <!-- Scripts -->
         @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/js/app.js', 'resources/css/app.css', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
-        @vite('resources/css/app.css')
-
     </head>
     <body class="font-sans antialiased">
         @inertia
